e0e0577b6f7b84dca7437cc0527f30f79ed0a9aa
Itamar Shefi <itamarshefi@gmail.com>
2020-11-04 11:49:23 +0200

    bing bot instead of google bot

--- background.js
+++ background.js
@@ -36,7 +36,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
     function(details) {
         for (var i = 0; i < details.requestHeaders.length; ++i) {
             if (details.requestHeaders[i].name === 'User-Agent') {
-                details.requestHeaders[i].value = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)';
+                details.requestHeaders[i].value = 'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)';
                 break;
             }
         }
