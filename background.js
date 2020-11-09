e4584056fdaa7564d3a4545522da5dcad8cac073
iddoyadlin <iddoyadlin@gmail.com>
2020-11-09 23:10:29 +0200

    changing to bingbot bumping version

--- background.js
+++ background.js
@@ -5,31 +5,6 @@ window.browser = (function () {
 })();
 
 
-// browser.runtime.onInstalled.addListener(function() {
-//     var isChrome = !!window.chrome && !!window.chrome.webstore;
-//     if (isChrome){
-//       chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-//         chrome.declarativeContent.onPageChanged.addRules([{
-//           conditions: [
-//           new chrome.declarativeContent.PageStateMatcher({
-//             pageUrl: {hostEquals: 'www.haaretz.co.il'},
-//          }),
-//           new chrome.declarativeContent.PageStateMatcher({
-//             pageUrl: {hostEquals: 'www.haaretz.com'},
-//          }),
-//           new chrome.declarativeContent.PageStateMatcher({
-//             pageUrl: {hostEquals: 'www.themarker.co.il'},
-//          }),
-//           new chrome.declarativeContent.PageStateMatcher({
-//             pageUrl: {hostEquals: 'www.quora.com'},
-//          })
-//           ],
-//           actions: [new chrome.declarativeContent.ShowPageAction()]
-//         }]);
-//       });
-//     }
-//   });
-
 browser.webRequest.onBeforeRequest.addListener(
   function() { return {cancel: true}; },
   {
@@ -43,7 +18,7 @@ browser.webRequest.onBeforeSendHeaders.addListener(
     function(details) {
         for (var i = 0; i < details.requestHeaders.length; ++i) {
             if (details.requestHeaders[i].name === 'User-Agent') {
-                details.requestHeaders[i].value = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)';
+                details.requestHeaders[i].value = 'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)';
                 break;
             }
         }
