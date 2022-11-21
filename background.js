98e5b2a777d13935feb7d56f4e423c20b301afca
arye321 <arye321@users.noreply.github.com>
2022-11-21 09:17:03 +0200

    updated user-agent

--- background.js
+++ background.js
@@ -36,7 +36,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
     function(details) {
         for (var i = 0; i < details.requestHeaders.length; ++i) {
             if (details.requestHeaders[i].name === 'User-Agent') {
-                details.requestHeaders[i].value = 'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)';
+                details.requestHeaders[i].value = 'Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)';
                 break;
             }
         }
